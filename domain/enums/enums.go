7b6d5b825ba1e4699a4ebd92ab5af01a33644077
ahargunyllib <billy.bpm03@gmail.com>
2025-01-19 00:46:03 +0700

    feat(enums): add activity types and corresponding calorie values

--- domain/enums/enums.go
+++ domain/enums/enums.go
@@ -1,14 +1,27 @@
 package enums
 
-type RoleEnum string
+var ActivityTypes = map[int16]string{
+	1:  "Walking",
+	2:  "Yoga",
+	3:  "Stretching",
+	4:  "Cycling",
+	5:  "Swimming",
+	6:  "Dancing",
+	7:  "Hiking",
+	8:  "Running",
+	9:  "HIIT",
+	10: "JumpRope",
+}
 
-const (
-	SuperAdmin RoleEnum = "Superadmin"
-	LeadAdmin  RoleEnum = "Lead Admin"
-	Admin      RoleEnum = "Admin"
-	User       RoleEnum = "User"
-)
-
-func (r RoleEnum) String() string {
-	return string(r)
+var Calories = map[string]int{
+	"Walking":    4,
+	"Yoga":       4,
+	"Stretching": 4,
+	"Cycling":    8,
+	"Swimming":   8,
+	"Dancing":    8,
+	"Hiking":     10,
+	"Running":    10,
+	"HIIT":       10,
+	"JumpRope":   10,
 }
